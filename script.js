028b4288ec37ebc5da686344f902f51908ab226b
Lililica <lucas.terrasson@gmail.com>
2026-06-01 22:09:04 +0200

    test sous projects

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function mapProjectsToHTML(containerId, projects) {
                     <h3>${project.title}</h3>
                     <p>${project.description}</p>
                 </div>
-                <a class="button button-primary" href="${project.link}">View Project</a>
+                ${project.link ? `<a class="button button-primary" href="${project.link}">View Project</a>` : `<a class="button button-secondary">Not Available</a>`}
             </article>
         `,
     )
@@ -26,12 +26,14 @@ let projects = [
         image: 'data/CellCenter2.png',
         description: 'Développement d\'outils de recherches sur OpenGL en C++. Étude autour des triangles de Delaunay et recherche d\'optimisation d\'énergie CVT. Relié au laboratoire de Vision par Ordinateur et Graphique.',
         tags: ['C++', 'Recherche', 'OpenGL'],
+        link : 'https://github.com/Lililica/CellsCenter',
     },
     {
         title: 'Ingénieur Logiciel C++ (UX & Interactions 3D)',
         image: 'data/DS.png',
         description: 'Implémenter un nouveau système de configuration de shortcut sur la 3DExperience. Intégration d’une UI complète, compréhension d’une codebase complexe, prise en compte des enjeux, organisation du projet.',
         tags: ['C++', 'JS', 'UI design', 'XML'],
+        private : true,
     },
 ];
 
@@ -41,10 +43,18 @@ mapProjectsToHTML('projPro', projects);
 
 projects = [
     {
+        title: 'Symphony',
+        image: 'data/Symphony.png',
+        description: 'GameJam 2026: Créer un jeux vidéo en 48h sur le thème : "Emmeler',
+        tags: ['C#', 'Unity', 'GameJam', 'Blender'],
+        link : 'projects/p1/project1.html',
+    },
+    {
         title: 'It’s Time !',
         image: 'data/time.png',
-        description: 'GameJam : Créer un jeux vidéo en 48h.',
+        description: 'GameJam 2025 : Créer un jeux vidéo en 48h sur le thème : "Profondeur".',
         tags: ['C++', 'Raylib', 'GameJam'],
+        link : 'https://github.com/Lililica/GameJame2024',
     },
     {
         title: 'Photographie',
@@ -60,10 +70,18 @@ mapProjectsToHTML('projPerso', projects);
 
 projects = [
     {
+        title: 'Raymarcher/Raytracer WebGL',
+        image: 'data/Raymarcher.png',
+        description: 'Ce projet explore deux approches différentes de rendu en temps réel: un raytracer et un raymarcher.',
+        tags: ['Synthése d’image', 'C++', 'OpenGL', 'ImGui', 'WebGL', 'JS', 'Raytracer', 'Raymarcher'],
+        link : 'https://lililica.github.io/ProjetPersoIMAC2026/index.html',
+    },
+    {
         title: 'Chess Game',
         image: 'data/chess2.png',
         description: 'Projet  : Créer un jeu d’échec en C++ avancé et en OpenGL + ImGui.',
         tags: ['Synthése d’image', 'C++', 'OpenGL', 'ImGui', 'Maths'],
+        link : 'https://github.com/Lililica/EchecProj-2025',
     },
     {
         title: 'Equations en image',
